2fb141df81a2cf6a446b83296796f566000780b5
Binhcool4 <binhtrieuhn99@gmail.com>
2024-12-02 22:50:36 +0700

    Update Btvn 5 Session10.c

--- Session 10/Btvn 5 Session10.c
+++ Session 10/Btvn 5 Session10.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	int i;
-	int j;
+    int i;
+    int j;
     int n;
     printf("Nhap kich thuoc mang: ");
     scanf("%d", &n);
@@ -19,8 +19,8 @@ int main() {
         for (j = 0; j < n - i - 1; j++)
             if (arr[j] > arr[j + 1]) { 
                 int temp = arr[j]; 
-				arr[j] = arr[j + 1]; 
-				arr[j + 1] = temp; 
+		arr[j] = arr[j + 1]; 
+		arr[j + 1] = temp; 
             }
 
     printf("Mang sau khi sap xep:\n");
