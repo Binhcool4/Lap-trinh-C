4abd33206dad8bac5866807438411cd177ed42a6
Binhcool4 <binhtrieuhn99@gmail.com>
2024-12-03 00:20:53 +0700

    Add files via upload

--- Session 10/Btvn 3 Session10.c
+++ Session 10/Btvn 3 Session10.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
 int main(){
-	int arr[] = {46, 26, 14, 24, 10};
+	int arr[] = {5, 10, 15, 6, 12};
 	int n = sizeof(arr)/sizeof(arr[0]);
 	int i;
 	int j;
 	
-	for (i = 0; i < n - 1; i++) {
-		int min_idx = i;
-		for (j = i + 1; j < n; j++) {
-		    if (arr[j] < arr[min_idx]) {
-		    min_idx = j;
-		}
-	}
-		int temp = arr[min_idx];
-		arr[min_idx] = arr[i];
-		arr[i] = temp;
-	
-	 }
-	
-	
-		printf("So nguyen to theo thu tu tang dan la : \n");
-		for ( i = 0; i < n; i++) {
-		    printf("%d ", arr[i]);
-		}
-		printf("\n");
+	for(i = 0; i<n; i++){
+		int key = arr[i];
+		int j = i - 1;
 		
+		while(j >= 0 && arr[j] > key){
+			arr[j+1] = arr[j];
+			j = j-1;
+		}
+		arr[j + 1] = key;
+	}
+	printf("So nguyen to theo thu tu tang dan la : \n");
+	for(i = 0; i < n; i++ ){
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	
 	return 0;
 }
-
