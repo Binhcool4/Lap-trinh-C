bc8377d7bca6e977b2f7c65a2a5888c696a6acff
Binhcool4 <binhtrieuhn99@gmail.com>
2024-12-09 17:04:50 +0700

    Add files via upload

--- Session 12/Btvn 3 Session12.c
+++ Session 12/Btvn 3 Session12.c
@@ -1,31 +1,18 @@
 #include <stdio.h>
-
-long long factorial(int n) {
-    if (n < 0) {
-        return -1;
-    }
-    
-    long long result = 1;
+int factorial(int n){
+    int result = 1;
     int i;
     for (i = 1; i <= n; i++) {
-        result *= i;  
+        result *= i;
     }
     return result;
 }
-
-int main() {
-    int number;
-    
+int main(){
+    int num;
     printf("Nhap mot so nguyen: ");
-    scanf("%d", &number);
-    
-    long long result = factorial(number);
-    if (result == -1) {
-        printf("Giai thua khong xac dinh voi so am.\n");
-    } else {
-        printf("Giai thua cua %d la: %lld\n", number, result);
-    }
-    
+    scanf("%d", &num);
+    int result = factorial(num);
+    printf("Giai thua cua %d la %d\n", num, result);
     return 0;
 }
 
